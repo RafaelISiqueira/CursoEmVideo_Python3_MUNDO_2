58f6dbc468422619e55c29a8f0f5da971161a793
RAFAEL LUIZ SIQUEIRA <Rafael.l.siqueira@hotmail.com>
2022-02-01 18:39:35 -0300

    Exercícios mundo 2

--- Ex047.py
+++ Ex047.py
@@ -3,14 +3,14 @@
 #1°: importar o sleep
 from time import sleep
 
-#2°: Laço da forma ''manual''
+#2°: RANGE de (1,51) se o numero é divisivel por 2 ele é par!
 #for n in range(1, 51):
  #   if n % 2 == 0:
    #     print(n, end=" ")
     #    sleep(0.4)
 #print("---!!!FIM!!---")
 
-#3°: Laço de repetição com ponto
+#3°: Laço de repetição iniciando no número 2, até 51, pulando de 2 em 2.
 for n in range(2, 51, 2):
     print('.', end=' ')
     print(n, end=" ")
