db461a1db10c45bac9baade08b52b8ec36f35b0a
RAFAEL LUIZ SIQUEIRA <Rafael.l.siqueira@hotmail.com>
2022-02-01 22:33:00 -0300

    Exercícios mundo 2

--- Ex055.py
+++ Ex055.py
@@ -10,5 +10,5 @@ for p in range(1, 6):
             maior = peso
         if peso < menor:
             menor = peso
-print("O MAIOR peso lido foi de {}Kg".format(maior))
+print("\nO MAIOR peso lido foi de {}Kg".format(maior))
 print("O MENOR peso lido foi de {}Kg".format(menor))
